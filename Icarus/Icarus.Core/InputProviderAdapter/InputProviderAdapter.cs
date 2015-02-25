ff49077a5cf73e97ebac34a03362017a52eaeda5
andrei jigoreanu <andrei.jigoreanu@softvision.ro>
2015-02-25 18:40:03 +0200

    New project structure

---
+++ Icarus/Icarus.Core/InputProviderAdapter/InputProviderAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Icarus.Core.Interfaces;
+
+namespace Icarus.Core.InputProviderAdapter
+{
+    class InputProviderAdapter
+    {
+        private readonly IInputProvider _inputProvider;
+
+        public InputProviderAdapter(IInputProvider inputProvider)
+        {
+            _inputProvider = inputProvider;
+        }
+    }
+}
