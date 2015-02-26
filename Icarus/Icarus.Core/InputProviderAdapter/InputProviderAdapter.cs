75d54c5feda35b99f5a184a4add7f2fd1876686e
Andrei Prisacaru <andrei.prisacaru@softvision.ro>
2015-02-26 12:37:18 +0200

    A bit of ....

--- Icarus/Icarus.Core/InputProviderAdapter/InputProviderAdapter.cs
+++ Icarus/Icarus.Core/InputProviderAdapter/InputProviderAdapter.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Icarus.Core.Interfaces;
+﻿using Icarus.Core.Interfaces;
 
 namespace Icarus.Core.InputProviderAdapter
 {
     class InputProviderAdapter
     {
-        private readonly IInputProvider _inputProvider;
+        private readonly IInputProvider inputProvider;
 
         public InputProviderAdapter(IInputProvider inputProvider)
         {
-            _inputProvider = inputProvider;
+            this.inputProvider = inputProvider;
         }
     }
 }
