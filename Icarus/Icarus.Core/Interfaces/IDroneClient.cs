079bd8de9d2756e8a4fab9bd0d8e689b4f7a7639
Andrei Prisacaru <andrei.prisacaru@softvision.ro>
2015-02-26 14:21:39 +0200

    Define Drone Client Interface

---
+++ Icarus/Icarus.Core/Interfaces/IDroneClient.cs
@@ -0,0 +1,23 @@
+﻿namespace Icarus.Core.Interfaces
+{
+    public interface IDroneClient
+    {
+        void Start();
+
+        void Stop();
+
+        void Configure();
+
+        void MoveUp();
+
+        void MoveDown();
+
+        void MoveForward();
+
+        void MoveBackward();
+
+        void MoveLeft();
+
+        void MoveRight();
+    }
+}
