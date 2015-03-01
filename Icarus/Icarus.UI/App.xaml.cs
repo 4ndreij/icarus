dadcb5e83a4e402b208e2d4a8124a7f475f60b05
Mihai Darie <mihai.darie@softvision.ro>
2015-03-01 20:21:56 +0200

    added structure map reference & bootstrapper

--- Icarus/Icarus.UI/App.xaml.cs
+++ Icarus/Icarus.UI/App.xaml.cs
@@ -7,5 +7,12 @@ namespace Icarus.UI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Bootstrap();
+            base.OnStartup(e);
+        }
+
     }
 }
---
+++ Icarus/Icarus.UI/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructureMap;
+
+namespace Icarus.UI
+{
+    public class Bootstrapper
+    {
+        public void Bootstrap()
+        {
+            //Container.For<ICommandFactory>().Use(CommandFactory);
+        }
+    }
+}
