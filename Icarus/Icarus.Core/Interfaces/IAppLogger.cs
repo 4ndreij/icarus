0e7ba96c960eb943dba80c2d532ed0763cf77e05
Mihai Darie <mihai.darie@softvision.ro>
2015-03-01 20:37:38 +0200

    added log4net reference and create logger wrapper

---
+++ Icarus/Icarus.Core/Interfaces/IAppLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icarus.Core.Interfaces
+{
+    public interface IAppLogger
+    {
+        void LogInfo(string subject, string message);
+        void LogError(string subject, string message, Exception ex);
+    }
+}
