c1fe15d14d60f610609bbbe75c89c704a351174b
Mihai Darie <mihai.darie@softvision.ro>
2015-03-09 22:21:22 +0200

    added MoveUpPressedTests and MoveUpReleasedTests

---
+++ Icarus/Icarus.Infrastructure.Tests/KeyboardInputProviderTests/MoveUpPressedTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Icarus.Infrastructure.KeyboardInputProvider;
+using Icarus.Infrastructure.KeyboardInputProvider.KeyboardHook;
+using Moq;
+using System.Windows.Input;
+
+namespace Icarus.Infrastructure.Tests.KeyboardInputProviderTests
+{
+    [TestFixture]
+    public class MoveUpPressedTests : BaseKeyboardTests
+    {
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+        }
+
+        [Test]
+        public void WhenPageUpKeyPressed_ShouldFireOnMoveUp()
+        {
+            // arrange
+            var wasCalled = false;
+            keyboardInputProvider.OnMoveUp += (o, e) => wasCalled = true;
+            bool isSysKey = false;
+            var keyArgs = new RawKeyEventArgs(KeyCodesHelper.PageUp, isSysKey, "PageUp");
+            keyArgs.Key = Key.PageUp;
+
+            // act
+            keyboardListenerMock.Raise(
+                x => x.KeyDown += null, keyArgs);
+
+            // assert
+            Assert.IsTrue(wasCalled);
+        }
+    }
+}
