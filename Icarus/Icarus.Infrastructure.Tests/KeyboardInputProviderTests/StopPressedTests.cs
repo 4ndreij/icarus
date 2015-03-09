86252f4f440f6cc00aeec6106937fea16a0d4563
Mihai Darie <mihai.darie@softvision.ro>
2015-03-09 22:31:37 +0200

    added StartPressedTests and StopPressedTests

---
+++ Icarus/Icarus.Infrastructure.Tests/KeyboardInputProviderTests/StopPressedTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Icarus.Infrastructure.KeyboardInputProvider;
+using Icarus.Infrastructure.KeyboardInputProvider.KeyboardHook;
+using Moq;
+using System.Windows.Input;
+
+namespace Icarus.Infrastructure.Tests.KeyboardInputProviderTests
+{
+    [TestFixture]
+    public class StopPressedTests : BaseKeyboardTests
+    {
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+        }
+
+        [Test]
+        public void WhenStopKeyPressed_ShouldFireOnStop()
+        {
+            // arrange
+            var wasCalled = false;
+            keyboardInputProvider.OnStop += (o, e) => wasCalled = true;
+            bool isSysKey = false;
+            var keyArgs = new RawKeyEventArgs(KeyCodesHelper.TKey, isSysKey, "Stop");
+            keyArgs.Key = Key.T;
+
+            // act
+            keyboardListenerMock.Raise(
+                x => x.KeyDown += null, keyArgs);
+
+            // assert
+            Assert.IsTrue(wasCalled);
+        }
+    }
+}
