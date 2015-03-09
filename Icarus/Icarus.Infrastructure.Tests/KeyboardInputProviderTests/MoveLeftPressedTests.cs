04ae828a3b1be85252c4e872f1f9288a153216d0
Mihai Darie <mihai.darie@softvision.ro>
2015-03-09 22:13:38 +0200

    added MoveLeftPressedTests and MoveLeftReleasedTests

---
+++ Icarus/Icarus.Infrastructure.Tests/KeyboardInputProviderTests/MoveLeftPressedTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Icarus.Infrastructure.KeyboardInputProvider;
+using Icarus.Infrastructure.KeyboardInputProvider.KeyboardHook;
+using Moq;
+using System.Windows.Input;
+
+namespace Icarus.Infrastructure.Tests.KeyboardInputProviderTests
+{
+    [TestFixture]
+    public class MoveLeftPressedTests : BaseKeyboardTests
+    {
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+        }
+
+        [Test]
+        public void WhenLeftArrowKeyPressed_ShouldFireOnMoveBackward()
+        {
+            // arrange
+            var wasCalled = false;
+            keyboardInputProvider.OnMoveLeft += (o, e) => wasCalled = true;
+            bool isSysKey = false;
+            var keyArgs = new RawKeyEventArgs(KeyCodesHelper.LeftArrow, isSysKey, "Left");
+            keyArgs.Key = Key.Left;
+
+            // act
+            keyboardListenerMock.Raise(
+                x => x.KeyDown += null, keyArgs);
+
+            // assert
+            Assert.IsTrue(wasCalled);
+        }
+    }
+}
---
+++ Icarus/Icarus.Infrastructure.Tests/KeyboardInputProviderTests/MoveLeftReleasedTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Icarus.Infrastructure.KeyboardInputProvider;
+using Icarus.Infrastructure.KeyboardInputProvider.KeyboardHook;
+using Moq;
+using System.Windows.Input;
+
+namespace Icarus.Infrastructure.Tests.KeyboardInputProviderTests
+{
+    [TestFixture]
+    public class MoveLeftReleasedTests : BaseKeyboardTests
+    {
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+        }
+
+        [Test]
+        public void WhenLeftArrowKeyReleased_ShouldFireOnMoveBackwardStopped()
+        {
+            // arrange
+            var wasCalled = false;
+            keyboardInputProvider.OnMoveLeftStopped += (o, e) => wasCalled = true;
+            bool isSysKey = false;
+            var keyArgs = new RawKeyEventArgs(KeyCodesHelper.LeftArrow, isSysKey, "Left");
+            keyArgs.Key = Key.Left;
+
+            // act
+            keyboardListenerMock.Raise(
+                x => x.KeyUp += null, keyArgs);
+
+            // assert
+            Assert.IsTrue(wasCalled);
+        }
+    }
+}
